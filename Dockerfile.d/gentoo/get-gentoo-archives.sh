2d5b131ad4219690b796f8b35be29fb51621818e
Hidenori Matsuki <dev@mazgi.com>
2020-11-05 04:43:13 +0000

    remove bash functions from script

--- Dockerfile.d/gentoo/get-gentoo-archives.sh
+++ Dockerfile.d/gentoo/get-gentoo-archives.sh
@@ -11,12 +11,12 @@ grep -vE '^\s*(#|$)' "${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}.txt" | awk '{ 
 basename $(cat "${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}-path.txt") > "${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}-filename.txt"
 
 # Download the stage3 archive.
-if [[ ! -f "${WORKING_DIR}/$(cat ${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}-filename.txt)" ]]; then
+if [ ! -f "${WORKING_DIR}/$(cat ${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}-filename.txt)" ]; then
   wget --directory-prefix "${WORKING_DIR}/" "${GENTOO_MIRROR}/releases/${GENTOO_ARCH}/autobuilds/$(cat ${WORKING_DIR}/latest-stage3-${GENTOO_ARCH}-path.txt)"
 fi
 
 # Download and extract portage repository archive.
 # see also: https://wiki.gentoo.org/wiki//var/db/repos/gentoo
-if [[ ! -f "${WORKING_DIR}/portage-latest.tar.xz" ]]; then
+if [ ! -f "${WORKING_DIR}/portage-latest.tar.xz" ]; then
   wget --directory-prefix "${WORKING_DIR}/" "${GENTOO_MIRROR}/snapshots/portage-latest.tar.xz"
 fi
